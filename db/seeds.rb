936c144a3c826db9fa6a4f1fd809cbc55ee51cbb
Dariusz Gertych <dariusz.gertych@gmail.com>
2021-07-29 16:21:41 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+imdb_ids = [
+  "tt0232500", "tt0322259", "tt0463985", "tt1013752",
+  "tt1596343", "tt1905041", "tt2820852", "tt4630562"
+]
+imdb_ids.each do |imdb_id|
+  Movies::Create.call(params: {imdb_id: imdb_id})
+end
+
+User.create([
+  {email: "owner@smallcinema.com", password: "P@ssw0rd!", role: "owner"},
+  {email: "moviegoer@smallcinema.com", password: "P@ssw0rd!", role: "moviegoer"}
+])
